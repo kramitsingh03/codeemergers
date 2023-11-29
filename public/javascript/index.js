bc116b37af47214524fc61c8e24b80fce567eb11
Amit <amitkumar03022004@gmail.com>
2023-11-30 04:34:17 +0530

    add qualification

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -24,9 +24,15 @@ function updateResume (){
     const OjobTitle = document.getElementById("o-job-title");
     const Ilink = document.getElementById("i-link").value;
     const OAlink = document.getElementById("o-a-link");
-
-    
-
+    const Ieducation = document.getElementById("i-education").value;
+    const Oeducation = document.getElementById("o-education");
+    const IstartMonth = document.getElementById("i-start-month").value;
+    const IgradMonth = document.getElementById("i-grad-month").value;
+    const Omonth = document.getElementById("o-month");
+    const Icourse = document.getElementById("i-course").value;
+    const Ocourse = document.getElementById("o-course");
+    const Ilocation = document.getElementById("i-location").value;
+    const Olocation = document.getElementById("o-location");
 
     Oname.innerText = Iname;
     Oemail.innerText = Iemail;
@@ -35,6 +41,10 @@ function updateResume (){
     OjobTitle.innerText = IjobTitle;
     OAlink.href = `https://${Ilink}`;
     OAlink.innerText = "Google";
+    Oeducation.innerText = Ieducation;
+    Omonth.textContent = `${IstartMonth} - ${IgradMonth}`;
+    Ocourse.innerText = Icourse;
+    Olocation.innerText = Ilocation;
 }
 
 
